d4f1b67ee92ab554e1633becc2e846f33202d5a4
Dmitry Chernikov <Dmitry Chernikov@DMCH>
2020-10-06 21:33:42 +0300

    Добавьте GITIGNORE- и GITATTRIBUTES-файлы.

---
+++ HomeWork_01/Models/ViewModelProduct.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeWork_01.Models
+{
+  public class ViewModelProduct
+  {
+
+    public static List<ViewModelProduct> GetData()
+    {
+      List<ViewModelProduct> data = new List<ViewModelProduct>();
+
+      int id = 0;
+      double price = 1000;
+      for(int i = 1; i < 10; i++)
+      {
+        id++;
+        price += 100;
+        string name = "Product " + i.ToString();
+        string fullName = "Product full name " + i.ToString();
+        string producer = "Producer " + i.ToString();
+
+        data.Add(new ViewModelProduct(id, name, fullName, producer, price));
+      }
+      return data;
+    }
+
+    public ViewModelProduct(int id, string name, string fullName, string producer, double price)
+    {
+      Id = id;
+      Name = name;
+      FullName = fullName;
+      Producer = producer;
+      Price = price;
+    }
+
+    public int Id { get; set; }
+
+    [Display(Description ="Наименование")]
+    public string Name { get; set; }
+
+    [Display(Description = "Полное наименование")]
+    public string FullName { get; set; }
+    public string Producer { get; set; }
+    public double Price { get; set; }
+
+  }
+}
---
+++ HomeWork_01/ViewControllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HomeWork_01.Models;
+
+namespace HomeWork_01.ViewControllers
+{
+  public class ProductController : Controller
+  {
+    public IActionResult List()
+    {
+      List<ViewModelProduct> data = ViewModelProduct.GetData();
+      return View(data);
+    }
+
+    public IActionResult Details(int id)
+    {
+      List<ViewModelProduct> data = ViewModelProduct.GetData();
+      var result = from item in data where item.Id == id select item;
+      if(result.Count() == 0) return NotFound("Продукт по Id = " + id.ToString() + " - не найден.");
+      return View(result.First());
+    }
+  }
+}
