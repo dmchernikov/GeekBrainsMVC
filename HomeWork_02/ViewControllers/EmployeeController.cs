ede015f78a5b9748d32f105d736288e63719c9a2
Дмитрий Черников <dmchernikov@mail.ru>
2020-10-14 15:24:38 +0300

    ch004

---
+++ HomeWork_02/ViewControllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HomeWork_02.Models;
+
+namespace HomeWork_02.ViewControllers
+{
+  public class EmployeeController : Controller
+  {
+    public IActionResult List()
+    {
+      List<ViewModelEmployee> data = ViewModelEmployee.GetData();
+      return View(data);
+    }
+
+    public IActionResult Details(int id)
+    {
+      List<ViewModelEmployee> data = ViewModelEmployee.GetData();
+      var result = from item in data where item.Id == id select item;
+      if(result.Count() == 0) return NotFound("Сотрудник по Id = " + id.ToString() + " - не найден.");
+      return View(result.First());
+    }
+  }
+}
